d61c2dc195b826a79c2ff708c041b639e3999d66
Stafford Williams <stafford.williams@gmail.com>
2023-06-19 22:12:06 +1000

    remove logging dir

--- library/train_util.py
+++ library/train_util.py
@@ -2980,9 +2980,7 @@ def prepare_accelerator(args: argparse.Namespace):
 
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
-        mixed_precision=args.mixed_precision,
-        log_with=log_with,
-        logging_dir=logging_dir,
+        mixed_precision=args.mixed_precision
     )
 
     # accelerateの互換性問題を解決する
